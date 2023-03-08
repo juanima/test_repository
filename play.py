9be4da5451f2550cd722350da8e556c5e7c92bdc
juanima <juan.urbano@mercadolibre.com.co>
2023-03-08 09:20:43 -0500

    good example

--- play.py
+++ play.py
@@ -24,6 +24,10 @@ def get_crypto(response, id, id1):
     if portfolio in response.user.portfolio.all():
         if response.method == "GET":
             crypto = portfolio.crypto_set.get(id=id1)
-            serializer = ItemSerializer(crypto)
+            if cryto in response.user.crypto.all():
+                serializer = ItemSerializer(crypto)
+            else:
+                return HTTPResponse("Unauthorized")
 
             return Response(serializer.data)
+
